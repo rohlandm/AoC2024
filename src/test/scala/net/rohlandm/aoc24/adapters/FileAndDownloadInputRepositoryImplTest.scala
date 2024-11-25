53151854621f1fc5ae6898983083c099fe6bd215
Maximilian Rohland <2794686+rohlandm@users.noreply.github.com>
2024-11-25 10:15:25 +0100

    === in test

--- src/test/scala/net/rohlandm/aoc24/adapters/FileAndDownloadInputRepositoryImplTest.scala
+++ src/test/scala/net/rohlandm/aoc24/adapters/FileAndDownloadInputRepositoryImplTest.scala
@@ -5,8 +5,8 @@ import org.scalatest.funsuite.AnyFunSuiteLike
 class FileAndDownloadInputRepositoryImplTest extends AnyFunSuiteLike {
 
   test("get Input for day 0 returns list of 3 Strings"):
-    assert(FileAndDownloadInputRepositoryImpl().getInput(0).size == 3)
-    assert(FileAndDownloadInputRepositoryImpl().getInput(0).head.equals("first Line"))
-    assert(FileAndDownloadInputRepositoryImpl().getInput(0).apply(1).equals("second Line"))
-    assert(FileAndDownloadInputRepositoryImpl().getInput(0).last.equals("another Line"))
+    assert(FileAndDownloadInputRepositoryImpl().getInput(0).size === 3)
+    assert(FileAndDownloadInputRepositoryImpl().getInput(0).head === "first Line")
+    assert(FileAndDownloadInputRepositoryImpl().getInput(0).apply(1) === "second Line")
+    assert(FileAndDownloadInputRepositoryImpl().getInput(0).last === "another Line")
 }
