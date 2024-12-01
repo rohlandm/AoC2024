3a983d40de3beaabb7360da33f94109e804e5dbb
Maximilian Rohland <2794686+rohlandm@users.noreply.github.com>
2024-12-01 10:41:26 +0100

    day 1 part 1

--- src/main/scala/net/rohlandm/aoc24/application/Day1Solver.scala
+++ src/main/scala/net/rohlandm/aoc24/application/Day1Solver.scala
@@ -3,7 +3,16 @@ package application
 
 import domain.DaySolver
 
+import scala.math.abs
+
 class Day1Solver extends DaySolver(1):
-  override def solvePart1(input: List[String]): Option[Long] = Some(1)
+  override def solvePart1(input: List[String]): Option[Long] =
+    val unzipped = input.map(parse).unzip
+    Some(unzipped._1.sorted.zip(unzipped._2.sorted).map((a, b) => abs(a - b)).sum)
 
   override def solvePart2(input: List[String]): Option[Long] = Some(2)
+
+  private val parse = (string: String) => string.split(" {3}").toList map (x => x.toInt) match
+    case List(a, b) => (a, b)
+    case _ => throw IllegalArgumentException()
+      
--- src/test/scala/net/rohlandm/aoc24/application/Day1SolverTest.scala
+++ src/test/scala/net/rohlandm/aoc24/application/Day1SolverTest.scala
@@ -7,8 +7,18 @@ import org.scalatest.funsuite.AnyFunSuite
  * Test cases for [[Day1Solver]]
  */
 class Day1SolverTest extends AnyFunSuite:
+
+  private val testInput =
+    """3   4
+      |4   3
+      |2   5
+      |1   3
+      |3   9
+      |3   3
+      |""".stripMargin.split("\n").toList
+
   test("solvePart1 returns result"):
-    assert(Day1Solver().solvePart1(List()) === Some(1))
+    assert(Day1Solver().solvePart1(testInput) === Some(11))
 
   test("solvePart2 returns result"):
-    assert(Day1Solver().solvePart2(List()) === Some(2))
+    assert(Day1Solver().solvePart2(testInput) === Some(2))
